c45195828b0ff991a30361ca36505bd98869466d
Harrisonk1 <66031262+Harrisonk1@users.noreply.github.com>
2020-07-14 21:44:42 -0400

    Query expenses based on month
    -added to the view function to query by month  too
    *function not working correctly in visual studio code but is in Python IDLE

--- MyMoney.py
+++ MyMoney.py
@@ -30,46 +30,44 @@ def log(amount, category, message=""):
     cur.execute(sql)
     conn.commit()
 
-def view(category=None):
+def view(category, date):
     conn = sqlite3.connect("spent.db")
     cur = conn.cursor()
-    if category:
+    if category.startswith('p'):
         sql = '''
-        select * from expenses where category = '{}'
-        '''.format(category)
+        select * from expenses where category = '{}' and date like '{}%'
+        '''.format(category, date)
         sql2 = '''
-        select sum(amount) from expenses where category = '{}'
-        '''.format(category)
+        select sum(amount) from expenses where category = '{}' and date like '{}%'
+        '''.format(category, date)
     else:
         sql = '''
-        select * from expenses
-        '''.format(category)
+        select * from expenses where date like '{}%'
+        '''.format(date)
         sql2 = '''
-        select sum(amount) from expenses
-        '''.format(category)
+        select sum(amount) from expenses where date like '{}%'
+        '''.format(date)
     cur.execute(sql)
     results = cur.fetchall()
     cur.execute(sql2)
     total_amount = cur.fetchone()[0]
-    
-    return total_amount, results
 
-
+  
 #Main loop for user input
 while True:
         print("What would you like to do?")
-        print("1 - Enter an expense\n2 - View expenses for this month\n3 - Update on spending habits\nQ - Quit")
+        print("1 - Enter an expense\n2 - View expenses based on date and category\n3 - Update on spending habits\nQ - Quit")
         ans = input(":")
 
         if ans == "1":
-            cost = input('What is the amount of the expense?')
-            cat = input('What is the category of the expense?')
-            msg = input('What is the expense for?')
+            cost = input('What is the amount of the expense?\n:')
+            cat = input('What is the category of the expense?\n:')
+            msg = input('What is the expense for?\n:')
             log(cost,cat,msg)
         elif ans == "2":
-            print('Enter what category of expenses you would like to view or press enter to view all')
-            cat = input(':')
-            print(view(cat))
+            ctg = input('Enter what category of expenses you would like to view or press enter to view all\n:')
+            dat = input('What month do you want to view? (yyyy-mm)\n:')
+            print(view(ctg,dat))
         elif ans == "3":
             compare()
         elif ans.lower() == "q":
